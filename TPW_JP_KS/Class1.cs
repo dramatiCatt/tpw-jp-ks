783377bfbad52314719c8757a683d181013142bf
PetraNovva <jula111002@gmail.com>
2023-03-09 12:58:20 +0100

    Start of the project
    Beggining of a project, for exercise 0

---
+++ TPW_JP_KS/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace TPW_JP_KS
+{
+    public class Calculator
+    {
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hejka, tu nasz kalkulator");
+        //}
+
+        public int sub(int a, int b)
+        {
+            a=10; b=5;
+            return a - b;
+        }
+
+        /*public int mul(int a, int b)
+        {
+            a = 10; b = 5;
+            return (a * b);
+        }
+
+        public int div(int a, int b)
+        {
+            a = 10; b = 5;
+            return a / b;
+        }*/
+    }
+}
