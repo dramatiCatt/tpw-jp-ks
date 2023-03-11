c6fcf47fada63b1f2f5d911a2b6888d738d78766
242530 Soltysiak Karolina <242530@edu.p.lodz.pl>
2023-03-11 19:37:03 +0100

    dodane testy, zmiany w class1

--- TPW_JP_KS/Class1.cs
+++ TPW_JP_KS/Class1.cs
@@ -2,27 +2,24 @@
 {
     public class Calculator
     {
-        //public static void Main(string[] args)
-        //{
-        //    Console.WriteLine("Hejka, tu nasz kalkulator");
-        //}
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hejka, tu nasz kalkulator");
+        }
 
         public int sub(int a, int b)
         {
-            a=10; b=5;
             return a - b;
         }
 
-        /*public int mul(int a, int b)
+        public int mul(int a, int b)
         {
-            a = 10; b = 5;
-            return (a * b);
+            return a * b;
         }
 
         public int div(int a, int b)
         {
-            a = 10; b = 5;
             return a / b;
-        }*/
+        }
     }
 }
